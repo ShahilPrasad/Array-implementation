2a513e5ed4bd7f4f9e6e722ba4444a73053236fd
Shahil Prasad <sprasad40@horizon.csueastbay.edu>
2022-06-25 04:04:20 -0700

    ewe

--- UnsortedList.cpp
+++ UnsortedList.cpp
@@ -1,1 +1,58 @@
 
+#include "UnsortedList.h"
+template<class T>
+UnsortedList<T>::UnsortedList() {
+    length = 0;
+    currentPos = 0;
+}
+template<class T>
+void UnsortedList<T>::MakeEmpty() {
+    length = 0;
+
+}
+template<class T>
+bool UnsortedList<T>::IsFull() const {
+
+    return length == MAX_ITEMS;
+}
+template<class T>
+int UnsortedList<T>::GetLength() const {
+    return 0;
+}
+template<class T>
+bool UnsortedList<T>::Contains(int someItem) {
+
+    for (int i = 0; i<length; i++){
+        if (arr[i] == someItem){
+            return true;
+        }
+    }
+    return false;
+}
+template<class T>
+void UnsortedList<T>::AddItem(int item) {
+    arr[length] = item;
+    length++;
+
+}
+template<class T>
+void UnsortedList<T>::DeleteItem(int item) {
+    for (int i = 0; i < length; i++){
+        if (arr[i] == item){
+            arr[i] = arr[length - 1];
+
+        }
+    }
+
+}
+template<class T>
+void UnsortedList<T>::ResetIterator() {
+    currentPos = 0; //keep tracks where we are
+
+
+}
+template<class T>
+int UnsortedList<T>::GetNextItem() {
+    return arr[currentPos++];
+}
+
